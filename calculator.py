669633b5a376c7542465aa040b90a80dc43548bf
Jessie Abraham <jessinj2@gmail.com>
2025-06-09 18:31:51 -0400

    update to test_calculator.py

--- calculator.py
+++ calculator.py
@@ -14,12 +14,14 @@ def divide(n1, n2):
 
     return n1/n2
 
-operations = {
-    "+": add, 
-    "-": substract,
-    "*": multiply,
-    "/": divide
-}
+if __name__ == "__main__":
+    operations = {
+        "+": add,
+        "-": substract,
+        "*": multiply,
+        "/": divide
+    }
+
 
 num1 = float(input("What is the first number: "))
 for symbol in operations:
--- test_calculator.py
+++ test_calculator.py
@@ -1,20 +1,28 @@
 import pytest
-from calculator import add, subtract, multiply, divide
+
+from calculator import add, substract, multiply, divide
 
 def test_add():
+
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
 
-def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(2, 5) == -3
+def test_substract(): 
+  
+    assert substract(5, 3) == 2
+    assert substract(2, 5) == -3
 
 def test_multiply():
+
     assert multiply(2, 3) == 6
     assert multiply(-2, 4) == -8
 
 def test_divide():
+
     assert divide(6, 3) == 2
     assert divide(5, 2) == 2.5
+
+    assert divide(0, 5) == 0
+   
     with pytest.raises(ValueError):
         divide(1, 0)
