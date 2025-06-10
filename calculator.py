2544f4b0f8ea9466062d1ec55b0a3b1122ade5a6
Jessie Abraham <jessinj2@gmail.com>
2025-06-09 20:40:01 -0400

    update to the calculator app

--- calculator.py
+++ calculator.py
@@ -8,11 +8,9 @@ def multiply(n1, n2):
     return n1 * n2
 
 def divide(n1, n2):
-    if n1 == 0 or n2==0 :
+    if n2 == 0: 
         raise ValueError("Cannot divide by zero")
     return n1 / n2
-
-    return n1/n2
 
 if __name__ == "__main__":
     operations = {
@@ -22,12 +20,20 @@ if __name__ == "__main__":
         "/": divide
     }
 
+    num1 = float(input("What is the first number: "))
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What is the second number: "))
+    
 
-num1 = float(input("What is the first number: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation: ")
-num2 = float(input("What is the second number: "))
-answer = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
+    calculation_function = operations.get(operation_symbol)
+    
+    if calculation_function:
+        try:
+            answer = calculation_function(num1, num2)
+            print(f"{num1} {operation_symbol} {num2} = {answer}")
+        except ValueError as e:
+            print(f"Error: {e}")
+    else:
+        print("Invalid operation selected.")
